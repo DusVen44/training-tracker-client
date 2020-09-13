8c721d65180d29be514f3b2cecf2feff53825bc3
Dustin Venable <Venny44@yahoo.com>
2020-09-13 13:26:23 -0500

    added heroku url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://training-tracker-api.herokuapp.com/',
+    API_ENDPOINT: 'https://training-tracker-api.herokuapp.com',
     TOKEN_KEY: 'training-tracker-client-auth-token',
     ID_KEY: 'user_id',
     USERNAME_KEY: 'username',
